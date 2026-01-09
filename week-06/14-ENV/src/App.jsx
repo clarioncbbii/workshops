886e6aebd600117c7e18770531fd55e1711f505c
Nene Nnoruka <237324102+clarioncbbii@users.noreply.github.com>
2026-01-09 16:30:14 +0000

    chore: routine upload

--- week-06/14-ENV/src/App.jsx
+++ week-06/14-ENV/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [otters, setOtters] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(import.meta.env.VITE_OTTER_API_URL);
+      const data = await response.json();
+      setOtters(data);
+    }
+
+    getData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Show Our Otter Data here</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+/*
+
+This is our FAKE API for this demo
+
+https://otters.com/api <-- returns an error
+
+e7f5ac3e1 is my secret key because I pay BIG bucks for this Otter API
+
+https://otters.com/api/e7f5ac3e1 <-- returns our data
+
+ */
